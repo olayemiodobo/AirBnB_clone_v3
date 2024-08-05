f69d74b398fc6101e4a51ab29feaa3c766557211
oladapsy <dapoodedeyi@yahoo.com>
2024-08-06 00:39:14 +0100

    test

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,20 @@
 """ the blueprint"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ return the status"""
     return jsonify({"status": "ok"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """an endpoint that retrieves the number of each objects by type:"""
+    return jsonify(amenities=storage.count('Amenity')
+                   cities=storage.count("City")
+                   places=storage.count("Place")
+                   reviews=storage.count("Review")
+                   states=storge.count("State")
+                   users=storage.count("User"))

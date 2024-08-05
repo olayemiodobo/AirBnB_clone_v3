68fabff693c1a3c0d344cb0ee95011b736d9cdb8
oladapsy <dapoodedeyi@yahoo.com>
2024-08-05 23:40:12 +0100

    api

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" the blueprint"""
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """ return the status"""
+    return jsonify({"status": "ok"})

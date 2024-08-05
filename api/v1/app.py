c05038130f759bab8fef263006b2814bd4ca41f2
oladapsy <dapoodedeyi@yahoo.com>
2024-08-06 00:26:33 +0100

    testing...

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 from api.v1.views import app_views
 from flask import Flask
 from models import storage
-import os
+from os import getenv
 """
     the module
 """
@@ -19,6 +19,6 @@ def teardown(exception):
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', '5000'))
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
